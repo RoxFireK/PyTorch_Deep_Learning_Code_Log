fd08e73a92b9fd330bd5a700335c28866e861e50
RoxChen <1572890271@qq.com>
2026-03-18 22:29:48 +0800

    Integrate TensorBoard for training and testing logs
    Added TensorBoard logging for training and testing metrics.

--- train01.py
+++ train01.py
@@ -1,8 +1,10 @@
 #先导入库再导入网络
 import torchvision
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
 #注意model文件要和main文件在同一文件夹下
-from model import *
+from model01 import *
 
 #准备数据集
 train_data = torchvision.datasets.CIFAR10("../dataset",train=True,transform = torchvision.transforms.ToTensor(),download = False)
@@ -38,10 +40,14 @@ total_test_step = 0
 #训练轮数
 epoch = 10
 
+#添加tensorboard
+writer = SummaryWriter("../logs_train")
+
 for i in range(epoch):
     print("epoch:{}".format(i+1))
 
     #训练步骤开始
+    rox.train()
     for data in train_dataloader:
         imgs,targets = data
         outputs = rox(imgs)
@@ -53,4 +59,32 @@ for i in range(epoch):
         optimizer.step()
 
         total_train_step = total_train_step + 1
-        print("训练次数:{},loss:{}".format(total_train_step,loss.item()))
+        if total_train_step % 100 == 0:
+            print("训练次数:{},loss:{}".format(total_train_step,loss.item()))
+            writer.add_scalar("train_loss",loss.item(),total_train_step)
+
+    #测试步骤开始 no_gard:没有梯度
+    rox.eval()
+    total_test_loss = 0
+    total_accuracy = 0
+    #不需要梯度来进行优化
+    with torch.no_grad():
+        for data in test_dataloader:
+            imgs,targets = data
+            outputs = rox(imgs)
+            loss =loss_fn(outputs,targets)
+            total_test_loss = total_test_loss + loss.item()
+            accuracy = (outputs.argmax(1) == targets).sum()
+            total_accuracy = total_accuracy + accuracy.item()
+    print("整体测试集上的loss:{}".format(total_test_loss))
+    print("整体测试集上的正确率:{}".format(total_accuracy/test_data_size))
+    writer.add_scalar("test_loss",total_test_loss,total_test_step)
+    writer.add_scalar("test_accuracy",total_accuracy/test_data_size,total_test_step)
+    total_train_step = total_train_step + 1
+
+    torch.save(rox,"rox_{}.pth".format(i))
+    print("模型已保存")
+
+writer.close()
+#访问 tensorboard --logdir=logs_train
+
